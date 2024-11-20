594d66586191fb652afa1ec214972267d247f22f
Leo <hiro.shaen@mail.ru>
2024-11-20 19:23:52 +0500

    [frontend] Первая классная кнопочка и toast

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="p-4">
+        <Toast />
         <h1>Welcome to my Nuxt 3 app!</h1>
         <h2>Redirect about page: {{ click }} </h2>
         <NuxtLink to="/about">
@@ -7,10 +8,15 @@
                 About
             </button>
         </NuxtLink>
+        <Button @click="show" label="Contrast" severity="contrast" />
     </div>
 </template>
 
 <script setup lang="ts">
+const toast = useToast();
+const show = () => {
+    toast.add({ severity: 'info', summary: 'Info', detail: 'HHFDHSFDKHSLKH', life: 3000 });
+};
 
 const click = useLocalStorage("index-click", 0)
 </script>
